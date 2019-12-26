e2acc3b5a5a1856556d3bda8fbfab9ed8c8cbe55
Lev Bandit Kaplunov <limon4ik59rus@mail.ru>
2019-12-26 16:47:55 +0500

    header 26.12.2019

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,14 @@
 $(function() {
 
-	// Custom JS
+	// Dropdown menu
+	$('.js-dropdown-open, .js-dropdown').hover( function(e) {
+		$(this).siblings('.js-dropdown').toggleClass('active');
+	});
 
+	$('.js-dropdown').hover( function(e) {
+		$(this).toggleClass('active');
+	});
+
+
+	
 });
